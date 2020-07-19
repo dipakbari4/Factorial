d378bdb025106377778620299a746c72f59ad18e
Dipak Bari <dipakbari4@gmail.com>
2020-07-20 00:02:34 +0530

    Factorial code written

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace Factorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int factorial = 1;
+
+            for(int i=1; i<=num; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            Console.WriteLine("Factorial of " + num + " is " + factorial);
         }
     }
 }
